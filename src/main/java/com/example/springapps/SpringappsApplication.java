820954e4b0cd1160413c2b70a8146b2543db222d
joapaul <joapaul@publicisgroupe.net>
2023-09-18 20:06:32 +0530

    test

--- src/main/java/com/example/springapps/SpringappsApplication.java
+++ src/main/java/com/example/springapps/SpringappsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringappsApplication {
+	//Test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappsApplication.class, args);
 	}
