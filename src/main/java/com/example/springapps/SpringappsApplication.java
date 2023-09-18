1e33c83f5d06aa23b675e138b8e7328fcf10adc4
joapaul <joapaul@publicisgroupe.net>
2023-09-18 19:48:29 +0530

    made name changes to workflowddd

--- src/main/java/com/example/springapps/SpringappsApplication.java
+++ src/main/java/com/example/springapps/SpringappsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringappsApplication {
-//This is main function
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappsApplication.class, args);
 	}
