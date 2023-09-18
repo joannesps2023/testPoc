650f71820b7d9138eddbb23cf4fd9e74800edb58
joapaul <joapaul@publicisgroupe.net>
2023-09-18 15:55:20 +0530

    first commit

---
+++ src/main/java/com/example/testPocapp/TestPocappApplication.java
@@ -0,0 +1,13 @@
+package com.example.testPocapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPocappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPocappApplication.class, args);
+	}
+
+}
